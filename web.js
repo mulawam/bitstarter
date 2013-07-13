198b428f614da093e32503346a9fe657059847f9
mulawam <max.mulawam@gmail.com>
2013-07-13 20:42:57 +0000

    Read from file

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var fs = require('fs');
+
+  fs.readFile('index.html', 'utf8', function (err,data) {response.send(data)})
+//eronse.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
